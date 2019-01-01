ca6f0d68aab9267053eabd1b99cc808004d78ce7
miao shan <minnieshan@gmail.com>
2019-01-01 16:08:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return Object.assign(object,{[key]: value})
 
 }
+
+function deleteFromObjectByKey(object, key){
+         
+  
+}
